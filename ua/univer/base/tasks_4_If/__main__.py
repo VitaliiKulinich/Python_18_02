abc46cbf3b1628142101484ec48283f01f88f420
VitaliiKulinich <vitalii.kulinich1@gmail.com>
2020-02-27 15:48:06 +0200

    3 homework Python_18_02_2020

--- ua/univer/base/tasks_4_If/__main__.py
+++ ua/univer/base/tasks_4_If/__main__.py
@@ -3,7 +3,7 @@ from ua.univer.base.tasks_4_If.tasks_if import task_1_if, count_positive_value, 
 
 if __name__ == "__main__":
     task_1_if()
-    count_positive_value(1,43,-42)
+    count_positive_value(1, 13, -42)
     count_negative_value(321, 32, -343)
     task_22_if()
     task_26_if()
