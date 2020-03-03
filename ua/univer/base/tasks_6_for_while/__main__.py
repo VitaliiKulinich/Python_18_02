cfd6514519fea2892e3be123f571cb6fc1376f6d
VitaliiKulinich <vitalii.kulinich1@gmail.com>
2020-03-03 13:38:32 +0200

    4 homework.

--- ua/univer/base/tasks_6_for_while/__main__.py
+++ ua/univer/base/tasks_6_for_while/__main__.py
@@ -1,4 +1,4 @@
-from ua.univer.base.tasks_6_for_while.tasks_for import task_20, task_12, task_36, task_33, task_5
+from ua.univer.base.tasks_6_for_while.tasks_while import task_1, task_4, task_7, task_12, task_23
 
 if __name__ == "__main__":
-    task_5()
+    task_30()
--- ua/univer/base/tasks_6_for_while/tasks_while.py
+++ ua/univer/base/tasks_6_for_while/tasks_while.py
@@ -1,1 +1,53 @@
-def task_5():
+def task_1():
+    a = 13
+    b = 2
+    i = 1
+    while i * b < a:
+        i += 1
+    print(i - 1)
+
+
+def task_4():
+    r = 1
+    s = 3
+    n = 9
+    while r * s < n:
+        r *= s
+    if r * s == n:
+        print("True")
+    else:
+        print("False")
+
+
+def task_7():
+    n = 8
+    k = 1
+    while k**2 < n:
+        k += 1
+    print("{0}**2 = {1} > {2}".format(k, k**2, n))
+
+
+def task_12():
+    k, n, suma = 1, 16, 1
+    while suma <= n:
+        k += 1
+        suma += k
+        if (suma + k) > n:
+            break
+    print(k, suma)
+
+
+def task_23():
+    a = 10
+    b = 6
+    while a != 0 and b != 0:
+        if a == b:
+            return print(a)
+        if a > b:
+            a = a % b
+        elif a < b:
+            b = b % a
+    if a != 0:
+        print(a)
+    else:
+        print(b)
